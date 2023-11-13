a73ff0b40baed1024a9b72c56a1ec2b1a92db39c
davidecardelli <davide.cardelli.94@gmail.com>
2023-11-13 15:39:34 +0100

    show

--- controllers/posts.js
+++ controllers/posts.js
@@ -10,15 +10,17 @@ const index = (req, res) => {
                 "<ul>",
                 ...posts.map(
                     (post) => `<li>
-              <h3>${post.title}</h3>
-              <img src="/img/posts/${post.image}" alt="${post.title}" style="max-width: 100px" />
-              <ul>
-                <li>${post.content}</li>
-                <li>
-                  <ol>${post.tags.map((tag) => `<li>${tag}</li>`).join("")}</ol>
-                </li>
-              </ul>
-            </li>`
+                        <h3>${post.title}</h3>
+                        <a href="/posts/${post.slug}" style="max-width: 100px">
+                            <img src="/img/posts/${post.image}" alt="${post.title}" style="max-width: 100px" />
+                        </a>
+                        <ul>
+                            <li>${post.content}</li>
+                            <li>
+                                <ol>${post.tags.map((tag) => `<li>${tag}</li>`).join("")}</ol>
+                            </li>
+                        </ul>
+                    </li>`
                 ),
                 "</ul>",
             ];
@@ -33,10 +35,18 @@ const index = (req, res) => {
 
 // ! show
 const show = (req, res) => {
+    const post = findOrFail(req, res);
     res.format({
         html: () => {
+            const htmlPost = `<h1>${post.title}</h1>
+            <img src="/img/posts/${post.image}" alt="${post.title}" style="max-width: 100%" />
+            <p>${post.content}</p>
+            <ul>${post.tags.map((tag) => `<li>${tag}</li>`).join("")}</ul>`;
+
+            res.type("html").send(htmlPost);
         },
         json: () => {
+            res.type("json").send({ post });
         },
     });
 };
@@ -60,5 +70,16 @@ const download = (req, res) => {
         },
     });
 };
+
+// ? Utils
+function findOrFail(req, res) {
+    const slug = req.params.slug;
+    const post = posts.find((post) => post.slug == slug);
+    if (!post) {
+        res.status(404).send(`Errore 404. Post con slug ${slug} non trovato`);
+        return;
+    }
+    return post;
+}
 
 module.exports = { index, show, create, download };
--- routers/posts.js
+++ routers/posts.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const postsController = require("../controllers/posts.js");
 
-router.get("/", postsController.index)
-router.get("/:slug", postsController.show)
-router.get("/creates", postsController.create)
-router.get("/:slug/download", postsController.download)
+router.get("/", postsController.index);
+router.get("/:slug", postsController.show);
+router.get("/create", postsController.create);
+router.get("/:slug/download", postsController.download);
 
 
 module.exports = router;
